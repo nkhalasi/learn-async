beb8b2fb31b3d7a32e994689cb1b56814e7200b5
Naresh Khalasi <khalasi@gmail.com>
2015-03-31 17:35:04 +0530

    Worked out the solution to flip just that first coin with heads between two tails or vice versa

--- flipcoin.py
+++ flipcoin.py
@@ -29,36 +29,22 @@ class Coin:
 def coin_states(coins):
     return [1 if c.heads else 0 for c in coins]
 
-def split_coins(coins):
-    heads = [c for c in coins if c.heads]
-    tails = [c for c in coins if c.tail]
-    return heads, tails
-
 def calculate_adjacency(coins):
     return sum([c.adjacency for c in coins])
 
-def flip_a_coin(flip_coin_with_heads):
-    def _flip_a_coin(coins):
-        flipped = False
-        for coin in coins:
-            if coin.previous_coin is None or coin.next_coin is None or flipped:
-                continue
-            if coin.heads != coin.previous_coin.heads:
-                if flip_coin_with_heads and coin.previous_coin.tail and coin.next_coin.tail and coin.heads:
-                    coin.flip()
-                    flipped = True
-                if not flip_coin_with_heads and coin.previous_coin.heads and coin.next_coin.heads and coin.tail:
-                    coin.flip()
-                    flipped = True
-    return _flip_a_coin
-
-flip_a_coin_with_heads = flip_a_coin(True)
-flip_a_coin_with_tail = flip_a_coin(False)
+def flip_coin(coins):
+    flipped = False
+    for coin in coins:
+        if coin.previous_coin is None or coin.next_coin is None or flipped:
+            continue
+        #if coin.heads != coin.previous_coin.heads and coin.previous_coin.heads == coin.next_coin.heads:
+        if coin.heads != coin.previous_coin.heads and coin.adjacency == 0:
+            coin.flip()
+            flipped = True
 
 if __name__ == '__main__':
     import random
     coin_sides = [random.randint(0,1) for _ in range(20)]
-    print(coin_sides)
     coins = []
     for i, cs in enumerate(coin_sides):
         if i == 0:
@@ -67,22 +53,12 @@ if __name__ == '__main__':
             coin = Coin(True if cs == 1 else False, coins[-1])
         coins.append(coin)
 
-    h, t = split_coins(coins)
-    ha, ta = calculate_adjacency(h), calculate_adjacency(t)
     print('Adjacencies before flipping')
     print(coin_states(coins))
-    print('Heads -> {}'.format(ha))
-    print('Tail -> {}'.format(ta))
+    print('Adjacency -> {}'.format(calculate_adjacency(coins)))
     print()
-    if ha < ta:
-        flip_a_coin_with_heads(coins)
-    else:
-        flip_a_coin_with_tail(coins)
-
-    h, t = split_coins(coins)
-    ha, ta = calculate_adjacency(h), calculate_adjacency(t)
+    flip_coin(coins)
     print('Adjacencies after flipping')
     print(coin_states(coins))
-    print('Heads -> {}'.format(ha))
-    print('Tail -> {}'.format(ta))
+    print('Adjacency -> {}'.format(calculate_adjacency(coins)))
     print()
