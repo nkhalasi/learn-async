9c85b97574beb5e0bc784c8713e72a01d5ae2487
Naresh Khalasi <khalasi@gmail.com>
2015-04-13 16:54:41 +0530

    Converted blocking actors to non-blocking by moving the Thread.sleep() into a Future

--- scala/src/main/scala/org/njk/actors/howdy/BarTender.scala
+++ scala/src/main/scala/org/njk/actors/howdy/BarTender.scala
@@ -1,18 +1,29 @@
 import akka.actor.{ActorLogging, Actor}
+import concurrent.Future
+import concurrent.ExecutionContext.Implicits.global
 
 class BarTender extends Actor with ActorLogging {
     var total = 0
     def receive = {
         case Ticket(quantity) =>
+            val s = sender
             total = total + quantity
 
-            log.info(s"I will get $quantity pints for [${sender.path}]")
+            log.info(s"I will get $quantity pints for [${s.path}]")
 
             for (number <- 1 to quantity) {
-                log.info(s"Pint $number is coming right up for [${sender.path}]")
-                Thread.sleep(1000)
-                log.info(s"Pint $number is ready, here you go [${sender.path}]")
-                sender ! FullPint(number)
+                log.info(s"Pint $number is coming right up for [${s.path}]")
+
+                //context.system.scheduler.scheduleOnce(1000 milliseconds) {
+                //    log.info(s"Pint $number is ready, here you go [${s.path}]")
+                //    s ! FullPint(number)
+                //}
+                
+                Future {
+                    Thread.sleep(1000)
+                    log.info(s"Pint $number is ready, here you go [${s.path}]")
+                    s ! FullPint(number)
+                }
             }
 
         case EmptyPint(number) =>
@@ -22,7 +33,7 @@ class BarTender extends Actor with ActorLogging {
                     context.system.shutdown()
                 case n =>
                     total = total - 1
-                    log.info("You drank pint $number quick, but there are still $total pints left")
+                    log.info(s"You drank pint $number quick, but there are still $total pints left")
             }
     }
 }
--- scala/src/main/scala/org/njk/actors/howdy/Person.scala
+++ scala/src/main/scala/org/njk/actors/howdy/Person.scala
@@ -1,12 +1,17 @@
 import akka.actor.{ActorLogging, Actor}
+import concurrent.Future
+import concurrent.ExecutionContext.Implicits.global
 
 class Person extends Actor with ActorLogging {
     def receive = {
         case FullPint(number) =>
+            val s = sender
             log.info(s"I will make short work of pint $number")
-            Thread.sleep(1000)
-            log.info(s"Done, here is the empty glass for pint $number")
-            sender ! EmptyPint(number)
+            Future {
+                Thread.sleep(1000)
+                log.info(s"Done, here is the empty glass for pint $number")
+                s ! EmptyPint(number)
+            }
     }
 }
 
